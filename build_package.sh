4a745ab9f1c20b035a80f79241455fa8f1e858d1
tony <tony@b-ad.ch>
2018-06-15 10:56:58 +0100

    fix build package

--- build_package.sh
+++ build_package.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+if [ ! $(which fpm) ]; then
+	echo "fpm is required to run debian packaging"
+	# make sure we are running in a legitimate user environment
+	ME=$(whoami)
+	if [[ "$ME" == "root" ]]; then
+		echo "please don't run me as root"
+		exit 1
+	fi
+
+	# Run script as sudo
+	sudo echo "thanks for admin priviliges"
+
+	apt-get install ruby ruby-dev rubygems build-essential
+	gem install --no-ri --no-rdoc fpm
+fi;
+
+if [ $(find -name "nonlibc_0.2.2_amd64.deb") ]; then
+	rm nonlibc_0.2.2_amd64.deb
+fi;
+
 # create the package
  fpm -s dir -t deb -n nonlibc -v 0.2.2 \
-	 src/libnonlibc.a=/usr/lib/x86_64-linux-gnu/libnonlibc.a \
-	 src/libnonlibc.so=/usr/lib/x86_64-linux-gnu/libnonlibc.so \
+	 src/libnonlibc.a=/usr/local/lib/linux-arm-gnueabihf/libnonlibc.a \
+	 src/libnonlibc.so=/usr/lib/linux-arm-gnueabihf/libnonlibc.so \
 	 util/fnvsum=/usr/local/bin/fnvsum \
 	 util/ncp=/usr/local/bin/ncp
